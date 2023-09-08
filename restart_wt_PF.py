386937af2a1b9481d7179dfee7a01f98ae2ef83f
Eric Samakinwa <57706825+shamakson@users.noreply.github.com>
2023-09-08 09:44:13 +0200

    Update restart_wt_PF.py
    A comprehensive code for processing and perturbing model data using particle filtering and EOF-based perturbations. This code involves several steps: 
    
    1. Selecting specific points over Europe and loading observation data. 
    2. Initializing particle filter parameters and iterating through Europe points to update particle weights. 
    3. Resampling particles based on their weights. 
    4. Selecting top particles with the largest average weights. 
    5. Perturbing the selected particles with model error by adding random noise. 
    6. Perturbing the data using the first ten EOF modes multiplied by random scalars.

--- restart_wt_PF.py
+++ restart_wt_PF.py
@@ -4,6 +4,8 @@ import pandas as pd
 from netCDF4 import Dataset
 from filterpy.monte_carlo import systematic_resample
 import shutil
+from scipy.linalg import svd
+import numpy.ma as ma 
 
 # Define the 10 points over Europe by longitude and latitude
 # Example latitude and longitude coordinates for testing (10 points over Europe)
@@ -54,7 +56,7 @@ for lat, lon in europe_points:
         # Update particle weights using a simple Gaussian likelihood
         distance = abs(observation_value - particle_value)
         weights[i] *= np.exp(-0.5 * (distance ** 2))
-        
+ 
         #print(f"Particle {i+1}: Weight = {weights[i]:.4f}")
     
     # Normalize the weights
@@ -71,8 +73,8 @@ for lat, lon in europe_points:
 # Calculate average weights
 average_weights = cumulative_weights / len(europe_points)
 
-# Select top 5 particles with largest average weights
-top_particle_indices = np.argsort(average_weights)[-5:][::-1]
+# Select top 7 particles with largest average weights
+top_particle_indices = np.argsort(average_weights)[-7:][::-1]
 
 print("\nTop 5 selected particles:")
 for idx, particle_index in enumerate(top_particle_indices):
@@ -84,8 +86,10 @@ for idx, particle_index in enumerate(top_particle_indices):
     input_model_file = model_data_paths[particle_index]
     output_file_name = os.path.basename(input_model_file).replace(".nc", f"_selected_{idx + 1}.nc")
     shutil.copy(input_model_file, output_file_name)
+    selected_files.append(output_file_name)
     print (f"Input file with largest weight {idx + 1} selected and copied")
 
+
 ##################################################################################################
 ##################################################################################################
 # Perturb the selected particles with model error
@@ -94,48 +98,92 @@ for idx, particle_index in enumerate(top_particle_indices):
 mu = 0.0  # Mean of model error perturbation
 sigma = 1.0  # Standard deviation of model error perturbation
 
-# Perturb the selected particles with model error
-mu = 0.0  # Mean of model error perturbation
-sigma = 1.0  # Standard deviation of model error perturbation
-
-# Define the model error value (replace 0.1 with your desired value)
-model_error_value = 0.1  # Adjust this value as needed
-
-# Loop through the selected files and perturb them
 for idx, selected_file in enumerate(selected_files):
-    selected_data_read = Dataset(selected_file)
+    selected_data_read = Dataset(selected_file, 'r+')
     
-    # Get latitude and longitude dimensions
-    latsdim = selected_data_read.dimensions['lat'].size
-    lonsdim = selected_data_read.dimensions['lon'].size
-    
-    # Create a new NetCDF file for the perturbed data
-    perturbed_output_file = f"perturbed_selected_{idx + 1}.nc"
-    perturbed_data = Dataset(perturbed_output_file, 'w', format='NETCDF4')
-    
-    # Create latitude and longitude dimensions
-    latdim = perturbed_data.createDimension('latitude', latsdim)
-    londim = perturbed_data.createDimension('longitude', lonsdim)
-    
-    # Create latitude and longitude variables
-    latitude = perturbed_data.createVariable('latitude', 'f8', ('latitude',))
-    longitude = perturbed_data.createVariable('longitude', 'f8', ('longitude',))
-    
-    # Copy latitude and longitude values from the original file
-    latitude[:] = selected_data_read.variables['lat'][:]
-    longitude[:] = selected_data_read.variables['lon'][:]
-    
-    # Create perturbed variable (assuming 'temp2' is the variable you want to perturb)
-    perturbed_temp2 = perturbed_data.createVariable('temp2', 'f4', ('latitude', 'longitude'))
-    
-    # Apply model error perturbation
-    perturbed_temp2[:, :] = selected_data_read.variables['temp2'][:] + model_error_value
-    
-    # Set attributes if needed
-    perturbed_temp2.long_name = "2m temperature"
-    perturbed_temp2.units = "K"
-    
-    # Close the perturbed NetCDF file
-    perturbed_data.close()
+    # Check if the file was successfully opened
+    if selected_data_read is not None and selected_data_read.isopen():
+        try:
+            # Get latitude and longitude dimensions from the original file
+            latsdim = selected_data_read.dimensions['lat'].size
+            lonsdim = selected_data_read.dimensions['lon'].size
 
-    print(f"Perturbed data saved to {perturbed_output_file}")
+            # Generate random white noise perturbation
+            random_perturbation = np.random.normal(loc=mu, scale=sigma, size=(latsdim, lonsdim))
+
+            # Modify 'temp2' variable with perturbation
+            selected_data_read.variables['temp2'][:] += random_perturbation
+
+            # Save the updated selected files with _perturbed suffix
+            perturbed_file_name = selected_file.replace(".nc", f"_perturbed_{idx + 1}.nc")
+            shutil.copy(selected_file, perturbed_file_name)
+            print(f"Perturbed data saved to {perturbed_file_name}")
+            
+            # Close the modified NetCDF file
+            selected_data_read.close()
+        except Exception as e:
+            print(f"An error occurred while processing {selected_file}: {str(e)}")
+            selected_data_read.close()  # Close the file in case of an error
+    else:
+        print(f"Error: Unable to open {selected_file}. Skipping...")
+
+##################################################################################################
+##################################################################################################
+# Perturbing the data using the first ten EOF modes multiplied by random scalars
+##################################################################################################
+##################################################################################################
+# Define the number of EOF modes to use for perturbation
+num_eof_modes = 10  # Adjust this as needed
+
+# Initialize an array to store random scaling factors for EOF modes
+random_scalars = np.random.normal(loc=0.0, scale=1.0, size=num_eof_modes)
+
+for idx, selected_file in enumerate(selected_files):
+    selected_data_read = Dataset(selected_file, 'r+')
+    
+    # Check if the file was successfully opened
+    if selected_data_read is not None and selected_data_read.isopen():
+        try:
+            # Load temperature data 'temp' from the current selected file
+            temp_data = selected_data_read.variables['temp2'][:]
+
+            # Convert masked array to regular array
+            if isinstance(temp_data, ma.MaskedArray):
+                temp_data = temp_data.filled()
+
+            # Flatten the temperature data to 2D (time, space)
+            temp_data_2d = temp_data.reshape(temp_data.shape[0], -1)
+
+            # Subtract the mean from each column (space) to obtain anomalies
+            anomalies = temp_data_2d - temp_data_2d.mean(axis=0)
+
+            # Perform Singular Value Decomposition (SVD)
+            U, S, VT = svd(anomalies, full_matrices=False)
+
+            # Use the first num_eof_modes EOF modes for perturbation
+            eof_modes_to_use = U[:, :num_eof_modes]
+
+            # Reshape random_scalars to have the shape (num_eof_modes, 1)
+            random_scalars_reshaped = random_scalars.reshape(-1, 1)
+
+            # Perturb the temperature data with EOF modes and random scalars
+            perturbed_temp_data = temp_data_2d + np.dot(eof_modes_to_use, random_scalars_reshaped)
+            
+            # Reshape the perturbed data back to its original shape
+            perturbed_temp_data = perturbed_temp_data.reshape(temp_data.shape)
+
+            # Update the 'temp' variable with the perturbed values
+            selected_data_read.variables['temp2'][:] = perturbed_temp_data
+
+            # Save the updated selected files with _perturbed_EOF suffix
+            perturbed_file_name = selected_file.replace(".nc", f"_perturbed_EOF_{idx + 1}.nc")
+            shutil.copy(selected_file, perturbed_file_name)
+            print(f"Perturbed data saved to {perturbed_file_name}")
+            
+            # Close the modified NetCDF file
+            selected_data_read.close()
+        except Exception as e:
+            print(f"An error occurred while processing {selected_file}: {str(e)}")
+            selected_data_read.close()  # Close the file in case of an error
+    else:
+        print(f"Error: Unable to open {selected_file}. Skipping...")
