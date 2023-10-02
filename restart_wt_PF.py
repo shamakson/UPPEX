8a44f80644bf1915225bddcc7185a4fc19f49793
Eric Samakinwa <57706825+shamakson@users.noreply.github.com>
2023-10-02 15:45:34 +0200

    Update restart_wt_PF.py
    Implementing for 50 particles and perturbing with mean EOF sum (10) added to a Gaussian random scalar

--- restart_wt_PF.py
+++ restart_wt_PF.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import pandas as pd
 from netCDF4 import Dataset
-from filterpy.monte_carlo import systematic_resample
 import shutil
 from scipy.linalg import svd
 import numpy.ma as ma 
@@ -21,15 +20,13 @@ europe_points = [
     (52.0116, 4.3571),# Delft, Netherlands
     (55.6761, 12.5683)   # Copenhagen, Denmark
 ]
-
 # Define the paths to the model_data files
-model_data_paths = [f"/scratch3/esamakin/particles/restart_WR_PF{str(i).zfill(3)}_17080103234500_echam.nc" for i in range(1, 21)]
+model_data_paths = [f"/scratch3/esamakin/particles/restart_WR_PF{str(i).zfill(3)}_17080103234500_echam.nc" for i in range(1, 51)]
 
 # Load the observation file using NetCDF4
-observation_path = "/scratch3/esamakin/ModE-Sim_set_1420-3_m041_1708_day_3.toasurf.nc"
+observation_path = "/scratch3/esamakin/ModE-Sim_set_1420-3_m041_1708_day_1.temp2.nc"
 observation_data = Dataset(observation_path)
 
-# Create a dataframe to store temperature values
 columns = ["lon", "lat", "obs"] + [f"particle{i:03d}" for i in range(1, 21)]
 df = pd.DataFrame(columns=columns)
 
@@ -54,11 +51,13 @@ for lat, lon in europe_points:
         particle_values.append(particle_value)
         
         # Update particle weights using a simple Gaussian likelihood
-        distance = abs(observation_value - particle_value)
-        weights[i] *= np.exp(-0.5 * (distance ** 2))
- 
-        #print(f"Particle {i+1}: Weight = {weights[i]:.4f}")
-    
+        #distance = abs(observation_value - particle_value)
+        #weights[i] *= np.exp(-0.5 * (distance ** 2))
+         
+         # Update particle weights using Euclidean distance
+        distance = np.sqrt((particle_value - observation_value) ** 2)
+        weights[i] *= 1.0 / (1.0 + distance)  
+
     # Normalize the weights
     weights /= np.sum(weights)
     cumulative_weights += weights
@@ -66,19 +65,20 @@ for lat, lon in europe_points:
     values = [lon, lat, observation_value] + particle_values
     df.loc[len(df)] = values
 
-    # Resample particles
-    resampled_indices = systematic_resample(weights)
-    particles_updated = [model_data_paths[i] for i in resampled_indices]
-
 # Calculate average weights
 average_weights = cumulative_weights / len(europe_points)
 
-# Select top 7 particles with largest average weights
-top_particle_indices = np.argsort(average_weights)[-7:][::-1]
+# Calculate the sum of all cumulative weights
+total_cumulative_weight = np.sum(cumulative_weights)
+# Normalize the cumulative weights
+cumulative_weights = cumulative_weights / total_cumulative_weight
 
-print("\nTop 5 selected particles:")
+# Select top 25 particles with largest average weights
+top_particle_indices = np.argsort(cumulative_weights)[-25:][::-1]
+
+print("\nTop 10 selected particles:")
 for idx, particle_index in enumerate(top_particle_indices):
-    print(f"Particle {idx+1}: Weight = {average_weights[particle_index]:.4f}")
+    print(f"Particle {idx+1}: Weight = {cumulative_weights[particle_index]:.4f}")
 
 # Now, copy the selected files with the largest average weights
 selected_files = []
@@ -87,103 +87,87 @@ for idx, particle_index in enumerate(top_particle_indices):
     output_file_name = os.path.basename(input_model_file).replace(".nc", f"_selected_{idx + 1}.nc")
     shutil.copy(input_model_file, output_file_name)
     selected_files.append(output_file_name)
-    print (f"Input file with largest weight {idx + 1} selected and copied")
+    print(f"Input file with largest weight {idx + 1} selected and copied")
 
+
+########################################################################################################
+########################################################################################################
+# Define the fill value for masked entries
+fill_value = -9999  
+# Combine all members of the model (temp2 variable)
+combined_temp2 = []
+for model_file in model_data_paths:
+    try:
+        model_data = Dataset(model_file)
+        temp2_values = model_data.variables['temp2'][:]
+        
+        # Fill masked values with the specified fill_value
+        temp2_values_filled = np.where(np.ma.getmask(temp2_values), fill_value, temp2_values)
+        
+        combined_temp2.append(temp2_values_filled)
+    except Exception as e:
+        print(f"An error occurred while processing {model_file}: {str(e)}")
+
+# Stack temp2 values along a new axis to combine them
+combined_temp2 = np.stack(combined_temp2, axis=0)
+# Ensure that combined_temp2 is a 2D array
+combined_temp2_2d = combined_temp2.reshape(combined_temp2.shape[0], -1)
+
+# Perform Singular Value Decomposition (SVD)
+U, S, VT = svd(combined_temp2_2d, full_matrices=False)
+
+# Number of EOF modes to use
+num_eof_modes = 10
+
+# Select the first num_eof_modes EOFs
+eof_modes_to_use = VT[:num_eof_modes, :]
+
+# Assuming temp2_values.shape is (lat, lon)
+lat, lon = temp2_values.shape
+
+# Reshape the EOF modes back to the grid shape
+eof_modes_grid_shape = eof_modes_to_use.reshape(num_eof_modes, lat, lon)
 
 ##################################################################################################
 ##################################################################################################
-# Perturb the selected particles with model error
+# Perturb the selected particles with model error 
 ##################################################################################################
 ##################################################################################################
 mu = 0.0  # Mean of model error perturbation
 sigma = 1.0  # Standard deviation of model error perturbation
+# Sum the first 10 EOFs on the grid
+sum_eof_modes_grid = np.mean(eof_modes_grid_shape, axis=0)
 
+# Perturb the selected files with model error 
 for idx, selected_file in enumerate(selected_files):
-    selected_data_read = Dataset(selected_file, 'r+')
-    
-    # Check if the file was successfully opened
-    if selected_data_read is not None and selected_data_read.isopen():
-        try:
-            # Get latitude and longitude dimensions from the original file
-            latsdim = selected_data_read.dimensions['lat'].size
-            lonsdim = selected_data_read.dimensions['lon'].size
+    try:
+        selected_data_read = Dataset(selected_file, 'r')
+        # Get latitude and longitude dimensions from the original file
+        latsdim = selected_data_read.dimensions['lat'].size
+        lonsdim = selected_data_read.dimensions['lon'].size
 
-            # Generate random white noise perturbation
-            random_perturbation = np.random.normal(loc=mu, scale=sigma, size=(latsdim, lonsdim))
-
-            # Modify 'temp2' variable with perturbation
-            selected_data_read.variables['temp2'][:] += random_perturbation
-
-            # Save the updated selected files with _perturbed suffix
-            perturbed_file_name = selected_file.replace(".nc", f"_perturbed_{idx + 1}.nc")
-            shutil.copy(selected_file, perturbed_file_name)
-            print(f"Perturbed data saved to {perturbed_file_name}")
-            
-            # Close the modified NetCDF file
-            selected_data_read.close()
-        except Exception as e:
-            print(f"An error occurred while processing {selected_file}: {str(e)}")
-            selected_data_read.close()  # Close the file in case of an error
+        
+        # Generate random white noise perturbation with the same shape as sum_eof_modes_grid
+        random_perturbation = np.random.normal(loc=mu, scale=sigma, size=(latsdim, lonsdim))
+        
+        # Create a copy of the selected file
+        perturbed_file_name = selected_file.replace(".nc", f"_perturbed_{idx + 1}.nc")
+        shutil.copy(selected_file, perturbed_file_name)
+        
+        # Open the copied file for modification
+        perturbed_data = Dataset(perturbed_file_name, 'r+')
+        
+        # Modify 'temp2' variable with perturbation
+        perturbation = sum_eof_modes_grid + random_perturbation
+        perturbed_data.variables['temp2'][:] += perturbation
+        
+        # Close the copied file
+        perturbed_data.close()
+        
+        print(f"Perturbed data saved to {perturbed_file_name}")
+        
+    except Exception as e:
+        print(f"An error occurred while processing {selected_file}: {str(e)}")
     else:
-        print(f"Error: Unable to open {selected_file}. Skipping...")
+        print(f"Successfully perturbed {selected_file} to {perturbed_file_name}.")
 
-##################################################################################################
-##################################################################################################
-# Perturbing the data using the first ten EOF modes multiplied by random scalars
-##################################################################################################
-##################################################################################################
-# Define the number of EOF modes to use for perturbation
-num_eof_modes = 10  # Adjust this as needed
-
-# Initialize an array to store random scaling factors for EOF modes
-random_scalars = np.random.normal(loc=0.0, scale=1.0, size=num_eof_modes)
-
-for idx, selected_file in enumerate(selected_files):
-    selected_data_read = Dataset(selected_file, 'r+')
-    
-    # Check if the file was successfully opened
-    if selected_data_read is not None and selected_data_read.isopen():
-        try:
-            # Load temperature data 'temp' from the current selected file
-            temp_data = selected_data_read.variables['temp2'][:]
-
-            # Convert masked array to regular array
-            if isinstance(temp_data, ma.MaskedArray):
-                temp_data = temp_data.filled()
-
-            # Flatten the temperature data to 2D (time, space)
-            temp_data_2d = temp_data.reshape(temp_data.shape[0], -1)
-
-            # Subtract the mean from each column (space) to obtain anomalies
-            anomalies = temp_data_2d - temp_data_2d.mean(axis=0)
-
-            # Perform Singular Value Decomposition (SVD)
-            U, S, VT = svd(anomalies, full_matrices=False)
-
-            # Use the first num_eof_modes EOF modes for perturbation
-            eof_modes_to_use = U[:, :num_eof_modes]
-
-            # Reshape random_scalars to have the shape (num_eof_modes, 1)
-            random_scalars_reshaped = random_scalars.reshape(-1, 1)
-
-            # Perturb the temperature data with EOF modes and random scalars
-            perturbed_temp_data = temp_data_2d + np.dot(eof_modes_to_use, random_scalars_reshaped)
-            
-            # Reshape the perturbed data back to its original shape
-            perturbed_temp_data = perturbed_temp_data.reshape(temp_data.shape)
-
-            # Update the 'temp' variable with the perturbed values
-            selected_data_read.variables['temp2'][:] = perturbed_temp_data
-
-            # Save the updated selected files with _perturbed_EOF suffix
-            perturbed_file_name = selected_file.replace(".nc", f"_perturbed_EOF_{idx + 1}.nc")
-            shutil.copy(selected_file, perturbed_file_name)
-            print(f"Perturbed data saved to {perturbed_file_name}")
-            
-            # Close the modified NetCDF file
-            selected_data_read.close()
-        except Exception as e:
-            print(f"An error occurred while processing {selected_file}: {str(e)}")
-            selected_data_read.close()  # Close the file in case of an error
-    else:
-        print(f"Error: Unable to open {selected_file}. Skipping...")
